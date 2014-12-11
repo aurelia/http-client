bd25426f85c98ee6ad0f24abcd6d666e71c43106
Rob Eisenberg <rob@bluespire.com>
2014-12-11 02:01:34 -0500

    fix(package): add es6-shim polyfill
    Depending on target browser(s), es6-shim may be required for Promise
    support.

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-path": "github:aurelia/path@0.0.1"
+    "aurelia-path": "github:aurelia/path@0.1.0",
+    "es6-shim": "github:paulmillr/es6-shim@0.21.1"
   }
 });
 
