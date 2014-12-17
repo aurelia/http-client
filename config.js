562b00bac62bb46e94d7db9df196d04350ee0f42
Rob Eisenberg <rob@bluespire.com>
2014-12-17 15:29:27 -0500

    fix(package): update dependencies to latest

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-path": "github:aurelia/path@0.1.0",
+    "aurelia-path": "github:aurelia/path@0.2.2",
     "es6-shim": "github:paulmillr/es6-shim@0.21.1"
   }
 });
