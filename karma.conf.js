6954c4fe74de49739cdab92e3753f418136d3b7f
Rob Eisenberg <rob@bluespire.com>
2014-12-17 15:34:27 -0500

    fix(*): switch http client to use path.join
    Previously the http client used path.normalize which would result in
    incorrect behavior if there was a baseAddress. This fix also corrects
    an error in the karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js': ['6to5'],
-      'lib/**/*.js': ['6to5']
+      'test/**/*.js': ['6to5'],
+      'src/**/*.js': ['6to5']
     },
 
 
--- src/http-client.js
+++ src/http-client.js
@@ -1,4 +1,4 @@
-import {normalize} from 'aurelia-path';
+import {join} from 'aurelia-path';
 import {HttpRequestMessage} from './http-request-message';
 import {HttpResponseMessage} from './http-response-message';
 import {JSONPRequestMessage} from './json-request-message';
@@ -16,35 +16,35 @@ export class HttpClient {
 
   get(uri){
     return this.send(
-      new HttpRequestMessage('GET',  normalize(uri, this.baseAddress))
+      new HttpRequestMessage('GET', join(this.baseAddress, uri))
         .withHeaders(this.defaultRequestHeaders)
       );
   }
 
   put(uri, content, replacer){
     return this.send(
-      new HttpRequestMessage('PUT', normalize(uri, this.baseAddress), content, replacer || this.replacer)
+      new HttpRequestMessage('PUT', join(this.baseAddress, uri), content, replacer || this.replacer)
         .withHeaders(this.defaultRequestHeaders)
       );
   }
 
   post(uri, content, replacer){
     return this.send(
-      new HttpRequestMessage('POST', normalize(uri, this.baseAddress), content, replacer || this.replacer)
+      new HttpRequestMessage('POST', join(this.baseAddress, uri), content, replacer || this.replacer)
         .withHeaders(this.defaultRequestHeaders)
       );
   }
 
   delete(uri){
     return this.send(
-      new HttpRequestMessage('DELETE', normalize(uri, this.baseAddress))
+      new HttpRequestMessage('DELETE', join(this.baseAddress, uri))
         .withHeaders(this.defaultRequestHeaders)
       );
   }
 
   jsonp(uri, callbackParameterName='jsoncallback'){
     return this.send(
-      new JSONPRequestMessage(normalize(uri, this.baseAddress), callbackParameterName)
+      new JSONPRequestMessage(join(this.baseAddress, uri), callbackParameterName)
       );
   }
 }
