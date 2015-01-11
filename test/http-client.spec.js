bf80cfd756c1e81ede1cd6f3213579d8e471b742
Martin Gustafsson <gusten86@gmail.com>
2015-01-11 16:10:46 +0100

    test(http-client): add unit tests for put, patch and post methods

--- test/http-client.spec.js
+++ test/http-client.spec.js
@@ -87,6 +87,285 @@ describe('http client', () => {
 
     }); 
 
+  describe('put', () => {
+
+    describe('request', () => { 
+
+      it('should make expected request', () => {    
+        var content = { firstName: "John", lastName: "Doe" };
+        var headers = new Headers();
+        headers.add('Content-Type', 'application/json');    
+        var client = new HttpClient(baseUrl, headers);
+
+        client.put('some/cool/path', content);
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        expect(request.url).toBe(`${baseUrl}some/cool/path`);      
+        expect(request.method).toBe('PUT');
+        expect(request.data()).toEqual(content);     
+      });  
+
+      it('should only include content properties specified in the replacer array', () => {    
+        var content = { firstName: "John", lastName: "Doe" };
+        var headers = new Headers();
+        headers.add('Content-Type', 'application/json');    
+        var client = new HttpClient(baseUrl, headers);
+
+        client.put('some/cool/path', content, ['firstName']);
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        expect(request.data()).not.toEqual(content); 
+        expect(request.data()).toEqual({ firstName: "John" });    
+      });  
+
+      it('should provide expected request headers', () => {      
+        var headers = new Headers();
+        headers.add('Authorization', 'bearer 123');
+        var client = new HttpClient(undefined, headers);
+
+        client.put('some/cool/path');
+
+        var request = jasmine.Ajax.requests.mostRecent();    
+
+        expect(request.requestHeaders['Authorization']).toBe('bearer 123');
+      });    
+
+    });
+
+    describe('response', () => {
+
+      it('should succeed on 200 response', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.get('some/cool/path').then(response => {
+          expect(response.isSuccess).toBe(true);
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 200 });
+      });
+
+      it('should retrive correct content', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.put('some/cool/path').then(response => {
+          expect(response.content.name).toBe('Martin');
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 200, responseText: '{"name":"Martin"}' });
+      });
+
+      it('should not succeed on 500 response', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.put('some/cool/path').then(response => {
+          expect(response.isSuccess).toBe(false);
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 500 });
+
+      }); 
+
+    }); 
+
+  });
+
+  describe('patch', () => {
+
+    describe('request', () => { 
+
+      it('should make expected request', () => {    
+        var content = { firstName: "John", lastName: "Doe" };
+        var headers = new Headers();
+        headers.add('Content-Type', 'application/json');    
+        var client = new HttpClient(baseUrl, headers);
+
+        client.patch('some/cool/path', content);
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        expect(request.url).toBe(`${baseUrl}some/cool/path`);      
+        expect(request.method).toBe('PATCH');
+        expect(request.data()).toEqual(content);     
+      });  
+
+      it('should only include content properties specified in the replacer array', () => {    
+        var content = { firstName: "John", lastName: "Doe" };
+        var headers = new Headers();
+        headers.add('Content-Type', 'application/json');    
+        var client = new HttpClient(baseUrl, headers);
+
+        client.patch('some/cool/path', content, ['firstName']);
+
+        var request = jasmine.Ajax.requests.mostRecent();
+    
+        expect(request.data()).not.toEqual(content); 
+        expect(request.data()).toEqual({ firstName: "John" });    
+      });  
+
+      it('should provide expected request headers', () => {      
+        var headers = new Headers();
+        headers.add('Authorization', 'bearer 123');
+        var client = new HttpClient(undefined, headers);
+
+        client.patch('some/cool/path');
+
+        var request = jasmine.Ajax.requests.mostRecent();    
+
+        expect(request.requestHeaders['Authorization']).toBe('bearer 123');
+      });    
+
+    });
+
+    describe('response', () => {
+
+      it('should succeed on 200 response', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.put('some/cool/path').then(response => {
+          expect(response.isSuccess).toBe(true);
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 200 });
+      });
+
+      it('should retrive correct content', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.patch('some/cool/path').then(response => {
+          expect(response.content.name).toBe('Martin');
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 200, responseText: '{"name":"Martin"}' });
+      });
+
+      it('should not succeed on 500 response', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.patch('some/cool/path').then(response => {
+          expect(response.isSuccess).toBe(false);
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 500 });
+
+      }); 
+
+    }); 
+
+  });
+
+describe('post', () => {
+
+    describe('request', () => { 
+
+      it('should make expected request', () => {    
+        var content = { firstName: "John", lastName: "Doe" };
+        var headers = new Headers();
+        headers.add('Content-Type', 'application/json');    
+        var client = new HttpClient(baseUrl, headers);
+
+        client.post('some/cool/path', content);
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        expect(request.url).toBe(`${baseUrl}some/cool/path`);      
+        expect(request.method).toBe('POST');
+        expect(request.data()).toEqual(content);     
+      });  
+
+      it('should only include content properties specified in the replacer array', () => {    
+        var content = { firstName: "John", lastName: "Doe" };
+        var headers = new Headers();
+        headers.add('Content-Type', 'application/json');    
+        var client = new HttpClient(baseUrl, headers);
+
+        client.post('some/cool/path', content, ['firstName']);
+
+        var request = jasmine.Ajax.requests.mostRecent();
+    
+        expect(request.data()).not.toEqual(content); 
+        expect(request.data()).toEqual({ firstName: "John" });    
+      });  
+
+      it('should provide expected request headers', () => {      
+        var headers = new Headers();
+        headers.add('Authorization', 'bearer 123');
+        var client = new HttpClient(undefined, headers);
+
+        client.post('some/cool/path');
+
+        var request = jasmine.Ajax.requests.mostRecent();    
+
+        expect(request.requestHeaders['Authorization']).toBe('bearer 123');
+      });    
+
+    });
+
+    describe('response', () => {
+
+      it('should succeed on 201 response', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.post('some/cool/path').then(response => {
+          expect(response.isSuccess).toBe(true);
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 201 });
+      });
+
+      it('should retrive correct content', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.post('some/cool/path').then(response => {
+          expect(response.content.name).toBe('Martin');
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 201, responseText: '{"name":"Martin"}' });
+      });
+
+      it('should not succeed on 500 response', (done) => {
+        var client = new HttpClient(baseUrl);
+
+        client.post('some/cool/path').then(response => {
+          expect(response.isSuccess).toBe(false);
+          done();
+        });
+
+        var request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith({ status: 500 });
+
+      }); 
+
+    }); 
+
+  });
+
   });
 
 });
