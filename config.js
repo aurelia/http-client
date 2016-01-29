9b0e4272ac4b784642d49aba87f206a949023868
Rob Eisenberg <rob@bluespire.com>
2016-01-29 12:51:06 -0500

    feat(package): update metadata for jspm; core-js; deps

--- config.js
+++ config.js
@@ -14,12 +14,12 @@ System.config({
   },
 
   map: {
-    "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.0.2",
-    "aurelia-pal-browser": "npm:aurelia-pal-browser@1.0.0-beta.1.0.3",
-    "aurelia-path": "npm:aurelia-path@1.0.0-beta.1",
+    "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
+    "aurelia-pal-browser": "npm:aurelia-pal-browser@1.0.0-beta.1.1.2",
+    "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.1.0",
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
-    "core-js": "npm:core-js@1.2.6",
+    "core-js": "npm:core-js@2.0.3",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -35,14 +35,14 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:aurelia-pal-browser@1.0.0-beta.1.0.3": {
-      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.0.2",
-      "core-js": "npm:core-js@1.2.6"
+    "npm:aurelia-pal-browser@1.0.0-beta.1.1.2": {
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
+      "core-js": "npm:core-js@2.0.3"
     },
     "npm:babel-runtime@5.8.34": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:core-js@1.2.6": {
+    "npm:core-js@2.0.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
