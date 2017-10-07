08783bb37b129b091bacb770bf2fb1ae3e20f1e6
EisenbergEffect <rob@bluespire.com>
2017-10-06 20:50:58 -0700

    chore(all): update dependencies

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ System.config({
   map: {
     "aurelia-pal": "npm:aurelia-pal@1.0.0",
     "aurelia-pal-browser": "npm:aurelia-pal-browser@1.0.0",
-    "aurelia-path": "npm:aurelia-path@1.0.0",
+    "aurelia-path": "npm:aurelia-path@1.1.1",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@2.4.1",
